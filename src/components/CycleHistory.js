9c82fb10109b75ab9bb65a4c51f20d05c37e5417
ZhoomartM <Joma1985mol@gmail.com>
2024-10-12 21:38:58 +0600

    first commit

---
+++ src/components/CycleHistory.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const CycleHistory = ({ history }) => {
+  return (
+    <div>
+      <h2>История циклов</h2>
+      <ul>
+        {history.map((cycle, index) => (
+          <li key={index}>
+            {`Цикл ${index + 1}: ${cycle.isWork ? "Работа" : "Отдых"}, длительность ${cycle.duration} минут`}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CycleHistory;
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Navbar.css';
+
+const Navbar = ({ onSettingsClick, onHistoryClick, showHistory }) => {
+
+ 
+
+  return (
+    <div className="navbar">
+    <button onClick={onSettingsClick}>Настройки</button>
+    <button onClick={onHistoryClick}>
+      {showHistory ? 'Скрыть историю' : 'Показать историю циклов'}
+    </button>
+    </div>
+  );
+};
+
+export default Navbar;
